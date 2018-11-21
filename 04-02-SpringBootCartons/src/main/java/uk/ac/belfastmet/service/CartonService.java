c84c39bf9a537e4163cdb730765a722540a80cb0
cal16135681 <cal16135681@CLCLRCAC11.belfastmet.int>
2018-11-21 16:17:34 +0000

    last line on Wednesday

---
+++ 04-02-SpringBootCartons/src/main/java/uk/ac/belfastmet/service/CartonService.java
@@ -0,0 +1,23 @@
+package uk.ac.belfastmet.service;
+
+import java.util.ArrayList;
+
+public class CartonService {
+	
+	public CartonService() {
+		super()
+	
+
+	public ArrayList<Carton> getToystoryCarton(){
+		
+		ArrayList<Carton> carton = new ArrayList<Carton>();
+		
+		<!-- here last day writing -->
+		
+		
+		}	
+	}
+	
+	
+	
+}
