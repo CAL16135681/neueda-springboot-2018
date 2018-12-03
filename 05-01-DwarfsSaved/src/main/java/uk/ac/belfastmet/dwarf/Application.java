a163ba9564735811839c1056586bd85d78de93cd
Gonzalo Calderon Garcia (CAL16135681) <CAL16135681@belfastmet.ac.uk>
2018-12-03 11:16:23 +0000

    Dwarfs ready for a database!

---
+++ 05-01-DwarfsSaved/src/main/java/uk/ac/belfastmet/dwarf/Application.java
@@ -0,0 +1,79 @@
+package uk.ac.belfastmet.dwarf;
+
+import java.util.ArrayList;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import uk.ac.belfastmet.dwarf.domain.Dwarf;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+			
+	}
+}	
+//	@PostConstruct
+//	public void createDwarfs() {
+//		
+//		ArrayList<Dwarf> dwarfs = new ArrayList<Dwarf>();
+//		
+//		Dwarf sleepy = new Dwarf("Sleepy", "Disney", "Sleepy.png");
+//		dwarfs.add(sleepy);
+//		Dwarf happy = new Dwarf("Happy", "Disney", "Happy.png");
+//		dwarfs.add(happy);
+//		Dwarf dopey = new Dwarf("Dopey", "Disney", "Dopey.png");
+//		dwarfs.add(dopey);
+//		Dwarf bashful = new Dwarf("Bashful", "Disney", "Bashful.png");
+//		dwarfs.add(bashful);
+//		Dwarf sneezy = new Dwarf("Sneezy", "Disney", "Sneezy.png");
+//		dwarfs.add(sneezy);
+//		Dwarf doc = new Dwarf("Doc", "Disney", "Doc.png");
+//		dwarfs.add(doc);
+//		Dwarf grmpy = new Dwarf("Grmpy", "Disney", "Grmpy.png");
+//		dwarfs.add(grmpy);
+//	
+//		
+//		Dwarf thorin = new Dwarf("Thorin", "Tolkien", "Thorin.png");
+//		Dwarf balin = new Dwarf("Balin", "Tolkien", "Balin.png");
+//		Dwarf dwalin = new Dwarf("Dwalin", "Tolkien", "Dwalin.png");
+//		Dwarf fili = new Dwarf("Fili", "Tolkien", "Fili.png");
+//		Dwarf kili = new Dwarf("Kili", "Tolkien", "Kili.png");
+//		Dwarf dori = new Dwarf("Dori", "Tolkien", "Dori.png");
+//		Dwarf nori = new Dwarf("Nori", "Tolkien", "Nori.png");
+//		Dwarf ori = new Dwarf("Ori", "Tolkien", "Ori.png");
+//		Dwarf oin = new Dwarf("Oin", "Tolkien", "Oin");
+//		Dwarf gloin = new Dwarf("Gloin", "Tolkien", "Gloin.png");
+//		Dwarf bifur = new Dwarf("Bifur", "Tolkien", "Bifur.png");
+//		Dwarf bofur = new Dwarf("Bofur", "Tolkien", "Bofur.png");
+//		Dwarf bomur = new Dwarf("Bomur", "Tolkien", "Bomur.png");
+//
+//		dwarfs.add(thorin);
+//		dwarfs.add(balin);
+//		dwarfs.add(dwalin);
+//		dwarfs.add(fili);
+//		dwarfs.add(kili);
+//		dwarfs.add(dori);
+//		dwarfs.add(nori);
+//		dwarfs.add(ori);
+//		dwarfs.add(oin);
+//		dwarfs.add(gloin);
+//		dwarfs.add(bifur);
+//		dwarfs.add(bofur);
+//		dwarfs.add(bomur);
+//		
+//		
+		
+		
+		
+		
+		
+		
+//	}
+	
+	
+	
