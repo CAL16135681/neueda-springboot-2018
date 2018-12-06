e32cf8c732ec32482db309eb055347997f4f816e
Gonzalo Calderon Garcia (CAL16135681) <CAL16135681@belfastmet.ac.uk>
2018-12-06 14:27:04 +0000

    06/12

--- 05-01-DwarfsSaved/src/main/java/uk/ac/belfastmet/dwarf/controller/DwarfController.java
+++ 05-01-DwarfsSaved/src/main/java/uk/ac/belfastmet/dwarf/controller/DwarfController.java
@@ -1,10 +1,14 @@
 package uk.ac.belfastmet.dwarf.controller;
 import java.util.ArrayList;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -41,7 +45,7 @@ public class DwarfController {
 		return "disney";
 	}
 	
-	//<!-- paso 3, lo puse aqui debajo-->
+	//<!-- 1.3-->
 	@GetMapping("/view/{dwarfId}")
 	public String viewDwarf(@PathVariable("dwarfId") Integer dwarfId,  Model model) { 
 		model.addAttribute("pageTitle", "View");
@@ -57,17 +61,34 @@ public class DwarfController {
 		return "dwarfPage";
 	}
 	
-	//<!--  2.1 -->
-	@GetMapping("/delete/{dwarfId}")
-	public String deleteDwarf(@PathVariable("dwarfId") Integer dwarfId, Model model, RedirectAttributes redirectAttributes) {
-		redirectAttributes.addFlashAttribute("message", "Succesfully deleted");
-		this.dwarfRepository.delete(dwarfId);
-		return "redirect:/dwarfs"
+	//<!--  1.3 thursday-->
+	@GetMapping("/add")
+	public String addDwarf(Model model) {
+		model.addAttribute("pageTitle", "Add");
+		model.addAttribute("dwarf", new Dwarf());
+	return "editDwarf";
+	}
+	
+	//<!--  1.3 thursday-->
+	@GetMapping("/edit/{dwarfId}")
+	public String editDwarf(@PathVariable ("dwarf") Integer dwarfId, Model model) {
+		model.addAttribute("pageTitle", "Edit");
+		model.addAttribute("dwarf", this.dwarfRepository.findByDwarfId(dwarfId));
+	return "editDwarf";
 	}
 	
 	
-	
-	
+	//<!--  1.3 thursday-->
+	@PostMapping("/save")
+	public String saveDwarf(@Valid Dwarf dwarf, BindingResult bindingResult, Model model) {
+		if(bindingResult.hasErrors())
+		{
+		return "editDwarf";
+		}else {
+		Dwarf savedDwarf = this.dwarfRepository.save(dwarf);
+		return "redirect:/view/"+ dwarf.getDwarfId();
+		}
+	}
 	
 	
 	
--- 05-02-TitanicCrud/src/main/java/uk/ac/belfastmet/titanic/controller/TitanicController.java
+++ 05-02-TitanicCrud/src/main/java/uk/ac/belfastmet/titanic/controller/TitanicController.java
@@ -1,11 +1,16 @@
 package uk.ac.belfastmet.titanic.controller;
 import java.util.ArrayList;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 import uk.ac.belfastmet.titanic.domain.Titanic;
 import uk.ac.belfastmet.titanic.repository.TitanicRepository;
@@ -15,16 +20,16 @@ import uk.ac.belfastmet.titanic.repository.TitanicRepository;
 @RequestMapping
 public class TitanicController {
 
-	TitanicRepository dwarfRepository; 
+	TitanicRepository titanicRepository; 
 
-	public TitanicController(TitanicRepository dwarfRepository) {
+	public TitanicController(TitanicRepository titanicRepository) {
 		super();
-		this.dwarfRepository = dwarfRepository;
+		this.titanicRepository = titanicRepository;
 	}
 
 
 	@GetMapping("/passengersPage")
-	public String disney(Model model) {
+	public String passengers(Model model) {
 		return "passengersPage";
 	}
 	
@@ -32,11 +37,29 @@ public class TitanicController {
 	@GetMapping("/view/{passengerid}")
 	public String viewPassengers(@PathVariable("passengerid") Integer passengerid,  Model model) { 
 		model.addAttribute("pageTitle", "View");
-		model.addAttribute("titanic",this.dwarfRepository.findBypassengerid(passengerid));
+		model.addAttribute("titanic",this.titanicRepository.findBypassengerid(passengerid));
 		return "viewPassengers";
 	}
 
+	
+	//<!--  1.3 thursday-->
+	@GetMapping("/add")
+	public String addPassenger(Model model) {
+		model.addAttribute("pageTitle", "Add");
+		model.addAttribute("titanic", new Titanic());
+	return "editPassengers";
+	}
 
+	@PostMapping("/save")
+	public String savePassenger(@Valid Titanic passenger, BindingResult bindingResult, Model model) {
+		if(bindingResult.hasErrors())
+		{
+		return "editPassengers";
+		}else {
+		Titanic savedPassenger = this.titanicRepository.save(passenger);
+		return "redirect:/view/"+ passenger.getPassengerid();
+		}
+	}
 	
 	
 }
